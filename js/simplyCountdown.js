7ba6d0dbd3e74c193c1b94ace59ee9e0cbaa4527
I339171 <rohith.m.k@sap.com>
2023-10-13 11:43:33 +0530

    initial counter

---
+++ js/simplyCountdown.js
@@ -0,0 +1,277 @@
+/*!
+ * Project : simply-countdown
+ * File : simplyCountdown
+ * Date : 27/06/2015
+ * License : MIT
+ * Version : 1.3.2
+ * Author : Vincent Loy <vincent.loy1@gmail.com>
+ * Contributors : 
+ *  - Justin Beasley <JustinB@harvest.org>
+ *  - Nathan Smith <NathanS@harvest.org>
+ */
+/*global window, document*/
+(function (exports) {
+    'use strict';
+
+    var // functions
+        extend,
+        createElements,
+        createCountdownElt,
+        simplyCountdown;
+
+    /**
+     * Function that merge user parameters with defaults one.
+     * @param out
+     * @returns {*|{}}
+     */
+    extend = function (out) {
+        var i,
+            obj,
+            key;
+        out = out || {};
+
+        for (i = 1; i < arguments.length; i += 1) {
+            obj = arguments[i];
+
+            if (obj) {
+                for (key in obj) {
+                    if (obj.hasOwnProperty(key)) {
+                        if (typeof obj[key] === 'object') {
+                            extend(out[key], obj[key]);
+                        } else {
+                            out[key] = obj[key];
+                        }
+                    }
+                }
+            }
+        }
+
+        return out;
+    };
+
+    /**
+     * Function that create a countdown section
+     * @param countdown
+     * @param parameters
+     * @param typeClass
+     * @returns {{full: (*|Element), amount: (*|Element), word: (*|Element)}}
+     */
+    createCountdownElt = function (countdown, parameters, typeClass) {
+        var innerSectionTag,
+            sectionTag,
+            amountTag,
+            wordTag;
+
+        sectionTag = document.createElement('div');
+        amountTag = document.createElement('span');
+        wordTag = document.createElement('span');
+        innerSectionTag = document.createElement('div');
+
+        innerSectionTag.appendChild(amountTag);
+        innerSectionTag.appendChild(wordTag);
+        sectionTag.appendChild(innerSectionTag);
+
+        sectionTag.classList.add(parameters.sectionClass);
+        sectionTag.classList.add(typeClass);
+        amountTag.classList.add(parameters.amountClass);
+        wordTag.classList.add(parameters.wordClass);
+
+        countdown.appendChild(sectionTag);
+
+        return {
+            full: sectionTag,
+            amount: amountTag,
+            word: wordTag
+        };
+    };
+
+    /**
+     * Function that create full countdown DOM elements calling createCountdownElt
+     * @param parameters
+     * @param countdown
+     * @returns {{days: (*|Element), hours: (*|Element), minutes: (*|Element), seconds: (*|Element)}}
+     */
+    createElements = function (parameters, countdown) {
+        var spanTag;
+
+        if (!parameters.inline) {
+            return {
+                years: createCountdownElt(countdown, parameters, 'simply-years-section'),
+                days: createCountdownElt(countdown, parameters, 'simply-days-section'),
+                hours: createCountdownElt(countdown, parameters, 'simply-hours-section'),
+                minutes: createCountdownElt(countdown, parameters, 'simply-minutes-section'),
+                seconds: createCountdownElt(countdown, parameters, 'simply-seconds-section')
+            };
+        }
+
+        spanTag = document.createElement('span');
+        spanTag.classList.add(parameters.inlineClass);
+        return spanTag;
+    };
+
+    /**
+     * simplyCountdown, create and display the coundtown.
+     * @param elt
+     * @param args (parameters)
+     */
+    simplyCountdown = function (elt, args) {
+        var parameters = extend({
+                year: 2023,
+                month: 9,
+                day: 25,
+                hours: 0,
+                minutes: 0,
+                seconds: 0,
+                words: {
+                    years: 'year',
+                    days: 'day',
+                    hours: 'hour',
+                    minutes: 'minute',
+                    seconds: 'second',
+                    pluralLetter: 's'
+                },
+                plural: true,
+                inline: false,
+                enableUtc: true,
+                onEnd: function () {
+                    return;
+                },
+                refresh: 1000,
+                inlineClass: 'simply-countdown-inline',
+                sectionClass: 'simply-section',
+                amountClass: 'simply-amount',
+                wordClass: 'simply-word',
+                zeroPad: false
+            }, args),
+            cd = document.querySelectorAll(elt);
+
+        Array.prototype.forEach.call(cd, function (countdown) {
+            var fullCountDown = createElements(parameters, countdown),
+                refresh;
+
+            refresh = function () {
+                var yearWord,
+                    dayWord,
+                    hourWord,
+                    minuteWord,
+                    secondWord;
+
+                var theEngagementDate = "Oct 26, 2023 11:47:00";
+                var theWeddingDate = "Jan 31, 2024 07:45:00";
+                
+                var today = new Date().getTime();
+                if(today < new Date(theEngagementDate).getTime()){
+                    var theDate = theEngagementDate;
+                    document.getElementById("whichEvent").innerHTML =  "We Are Getting Engaged";
+                    document.getElementById("inviteDatePlace").innerHTML = "October 26th, 2023 Aluva, Kerala";
+                    document.getElementById("eventInvitation").innerHTML = "We invited you to celebrate our engagement";
+                } else {
+                    document.getElementById("whichEvent").innerHTML =  "We Are Getting Married";
+                    document.getElementById("inviteDatePlace").innerHTML = "January 31th, 2024 Aluva, Kerala";
+                    document.getElementById("eventInvitation").innerHTML = "We invited you to celebrate our wedding.";
+                    theDate = theWeddingDate;
+                }
+                
+                var countTo = new Date(theDate).getTime();
+                var now = new Date(),
+                    countTo = new Date(countTo),
+                    timeDifference = (countTo - now);
+                
+                if(timeDifference < 0){
+                    var countFrom = new Date(theWeddingDate),
+                        timeDifference = (now - countFrom);
+                    document.getElementById("whichEvent").innerHTML =  "We Are Being Married For";
+                    document.getElementById("inviteDatePlace").innerHTML = "";
+                    document.getElementById("eventInvitation").innerHTML = "";
+                }
+                    
+                var secondsInADay = 60 * 60 * 1000 * 24,
+                    secondsInAHour = 60 * 60 * 1000;
+
+                
+                var days = Math.floor(timeDifference / (secondsInADay) * 1);
+                var years = Math.floor(days / 365);
+                if (years >= 1){ days = days - (years * 365) }
+                var hours = Math.floor((timeDifference % (secondsInADay)) / (secondsInAHour) * 1);
+                var minutes = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1);
+                var seconds = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
+
+                if (parameters.plural) {
+                    yearWord = years > 1
+                    ? parameters.words.years + parameters.words.pluralLetter
+                    : parameters.words.years;
+
+                    dayWord = days > 1
+                        ? parameters.words.days + parameters.words.pluralLetter
+                        : parameters.words.days;
+
+                    hourWord = hours > 1
+                        ? parameters.words.hours + parameters.words.pluralLetter
+                        : parameters.words.hours;
+
+                    minuteWord = minutes > 1
+                        ? parameters.words.minutes + parameters.words.pluralLetter
+                        : parameters.words.minutes;
+
+                    secondWord = seconds > 1
+                        ? parameters.words.seconds + parameters.words.pluralLetter
+                        : parameters.words.seconds;
+
+                } else {
+                    yearWord = parameters.words.years; 
+                    dayWord = parameters.words.days;
+                    hourWord = parameters.words.hours;
+                    minuteWord = parameters.words.minutes;
+                    secondWord = parameters.words.seconds;
+                }
+
+                /* display an inline countdown into a span tag */
+                if (parameters.inline) {
+                    countdown.innerHTML =
+                        years + ' ' + yearWord +', ' +
+                        days + ' ' + dayWord + ', ' +
+                        hours + ' ' + hourWord + ', ' +
+                        minutes + ' ' + minuteWord + ', ' +
+                        seconds + ' ' + secondWord + '.';
+
+                } else {
+                    fullCountDown.years.amount.textContent = (parameters.zeroPad && years.toString().length < 2 ? '0' : '') + years;
+                    fullCountDown.years.word.textContent = yearWord;
+
+                    fullCountDown.days.amount.textContent = (parameters.zeroPad && days.toString().length < 2 ? '0' : '') + days;
+                    fullCountDown.days.word.textContent = dayWord;
+
+                    fullCountDown.hours.amount.textContent = (parameters.zeroPad && hours.toString().length < 2 ? '0' : '') + hours;
+                    fullCountDown.hours.word.textContent = hourWord;
+
+                    fullCountDown.minutes.amount.textContent = (parameters.zeroPad && minutes.toString().length < 2 ? '0' : '') + minutes;
+                    fullCountDown.minutes.word.textContent = minuteWord;
+
+                    fullCountDown.seconds.amount.textContent = (parameters.zeroPad && seconds.toString().length < 2 ? '0' : '') + seconds;
+                    fullCountDown.seconds.word.textContent = secondWord;
+                }
+            };
+
+            // Refresh immediately to prevent a Flash of Unstyled Content
+            refresh();
+            interval = window.setInterval(refresh, parameters.refresh);
+        });
+    };
+
+    exports.simplyCountdown = simplyCountdown;
+}(window));
+
+/*global $, jQuery, simplyCountdown*/
+if (window.jQuery) {
+    (function ($, simplyCountdown) {
+        'use strict';
+
+        function simplyCountdownify(el, options) {
+            simplyCountdown(el, options);
+        }
+
+        $.fn.simplyCountdown = function (options) {
+            return simplyCountdownify(this.selector, options);
+        };
+    }(jQuery, simplyCountdown));
+}
